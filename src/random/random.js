91101ebd0e0ed0c37123710d3e4c5703c63e925b
Artem S Vybornov <vybornov@gmail.com>
2014-04-12 13:17:01 +0400

    `crypto.getRandomValues` gracefully falls back to `Math.random`

--- src/random/random.js
+++ src/random/random.js
@@ -1,9 +1,20 @@
+var _global_crypto = global.crypto;
+
 function Random_getBytes ( buffer ) {
     if ( !is_buffer(buffer) && !is_bytes(buffer) )
         throw new TypeError("unexpected buffer type");
 
     var bytes = new Uint8Array( (buffer.buffer||buffer), buffer.byteOffset||0, buffer.byteLength||buffer.length );
-    global.crypto.getRandomValues(bytes);
+
+    if ( _global_crypto ) {
+        _global_crypto.getRandomValues(bytes);
+    }
+    else {
+        var random = global.Math.random;
+        for ( var i = 0; i < bytes.length; i++ ) {
+            bytes[i] = random() * 256 | 0;
+        }
+    }
 
     return bytes;
 }
