f7affadee3cfea5a5c0508d8cfbd82b740a2ee6b
Artem S Vybornov <vybornov@gmail.com>
2014-05-19 03:58:59 +0400

    ISAAC: gracefully avoid loosing of seed entropy on biased seeds (not finished)
    https://github.com/vibornoff/asmcrypto.js/issues/27#issuecomment-43456230

--- src/random/isaac.js
+++ src/random/isaac.js
@@ -57,22 +57,7 @@ var ISAAC = ( function () {
 
     /* public: seeding function */
     function seed ( s ) {
-        var a, b, c, d, e, f, g, h, i, n;
-
-        // the golden ratio
-        a = b = c = d = e = f = g = h = 0x9e3779b9;
-
-        // scramble it
-        for ( i = 0; i < 4; i++ ) {
-            a ^= b <<  11; d = (d + a)|0; b = (b + c)|0;
-            b ^= c >>>  2; e = (e + b)|0; c = (c + d)|0;
-            c ^= d <<   8; f = (f + c)|0; d = (d + e)|0;
-            d ^= e >>> 16; g = (g + d)|0; e = (e + f)|0;
-            e ^= f <<  10; h = (h + e)|0; f = (f + g)|0;
-            f ^= g >>>  4; a = (a + f)|0; g = (g + h)|0;
-            g ^= h <<   8; b = (b + g)|0; h = (h + a)|0;
-            h ^= a >>>  9; c = (c + h)|0; a = (a + b)|0;
-        }
+        var a, b, c, d, e, f, g, h, i, j, k, n, l;
 
         if ( !is_typed_array(s) ) {
             if ( is_number(s) ) {
@@ -99,67 +84,81 @@ var ISAAC = ( function () {
         reset();
 
         // compress the seed
-        for ( i = 0, n = 0; i < s.length; i++ ) {
-            n <<= 4, n |= s[i];
-            if ( (i & 3) !== 3 ) continue;
-            r[(i >> 2) & 255] ^= n;
-            n = 0;
-        }
-        if ( (i & 3) !== 0 ) {
-            r[(i >> 2) & 255] ^= n;
-            n = 0;
-        }
+        l = s.length;
+        for ( j = 0; j < l; j += 1024 )
+        {
+            // process seed chunk, pad with zeros up to 1024 octets
+            for ( k = j, i = 0; i < 1024; k = j + (++i) | 0 ) {
+                n <<= 4, n |= (k < l) ? s[k] : 0;
+                r[(k >> 2) & 255] = n, n = 0;
+            }
 
-        // mix it
-        for ( i = 0; i < 256; i += 8 ) {
-            a = (a + r[i|0])|0; b = (b + r[i|1])|0;
-            c = (c + r[i|2])|0; d = (d + r[i|3])|0;
-            e = (e + r[i|4])|0; f = (f + r[i|5])|0;
-            g = (g + r[i|6])|0; h = (h + r[i|7])|0;
+            // the golden ratio
+            a = b = c = d = e = f = g = h = 0x9e3779b9;
 
-            a ^= b <<  11; d = (d + a)|0; b = (b + c)|0;
-            b ^= c >>>  2; e = (e + b)|0; c = (c + d)|0;
-            c ^= d <<   8; f = (f + c)|0; d = (d + e)|0;
-            d ^= e >>> 16; g = (g + d)|0; e = (e + f)|0;
-            e ^= f <<  10; h = (h + e)|0; f = (f + g)|0;
-            f ^= g >>>  4; a = (a + f)|0; g = (g + h)|0;
-            g ^= h <<   8; b = (b + g)|0; h = (h + a)|0;
-            h ^= a >>>  9; c = (c + h)|0; a = (a + b)|0;
+            // scramble it
+            for ( i = 0; i < 4; i++ ) {
+                a ^= b <<  11; d = (d + a)|0; b = (b + c)|0;
+                b ^= c >>>  2; e = (e + b)|0; c = (c + d)|0;
+                c ^= d <<   8; f = (f + c)|0; d = (d + e)|0;
+                d ^= e >>> 16; g = (g + d)|0; e = (e + f)|0;
+                e ^= f <<  10; h = (h + e)|0; f = (f + g)|0;
+                f ^= g >>>  4; a = (a + f)|0; g = (g + h)|0;
+                g ^= h <<   8; b = (b + g)|0; h = (h + a)|0;
+                h ^= a >>>  9; c = (c + h)|0; a = (a + b)|0;
+            }
 
-            m[i|0] = a,
-            m[i|1] = b,
-            m[i|2] = c,
-            m[i|3] = d,
-            m[i|4] = e,
-            m[i|5] = f,
-            m[i|6] = g,
-            m[i|7] = h;
-        }
+            // mix it and combine with the internal state
+            for ( i = 0; i < 256; i += 8 ) {
+                a = (a + r[i|0])|0; b = (b + r[i|1])|0;
+                c = (c + r[i|2])|0; d = (d + r[i|3])|0;
+                e = (e + r[i|4])|0; f = (f + r[i|5])|0;
+                g = (g + r[i|6])|0; h = (h + r[i|7])|0;
 
-        // mix it again
-        for ( i = 0; i < 256; i += 8 ) {
-            a = (a + m[i|0])|0; b = (b + m[i|1])|0;
-            c = (c + m[i|2])|0; d = (d + m[i|3])|0;
-            e = (e + m[i|4])|0; f = (f + m[i|5])|0;
-            g = (g + m[i|6])|0; h = (h + m[i|7])|0;
+                a ^= b <<  11; d = (d + a)|0; b = (b + c)|0;
+                b ^= c >>>  2; e = (e + b)|0; c = (c + d)|0;
+                c ^= d <<   8; f = (f + c)|0; d = (d + e)|0;
+                d ^= e >>> 16; g = (g + d)|0; e = (e + f)|0;
+                e ^= f <<  10; h = (h + e)|0; f = (f + g)|0;
+                f ^= g >>>  4; a = (a + f)|0; g = (g + h)|0;
+                g ^= h <<   8; b = (b + g)|0; h = (h + a)|0;
+                h ^= a >>>  9; c = (c + h)|0; a = (a + b)|0;
 
-            a ^= b <<  11; d = (d + a)|0; b = (b + c)|0;
-            b ^= c >>>  2; e = (e + b)|0; c = (c + d)|0;
-            c ^= d <<   8; f = (f + c)|0; d = (d + e)|0;
-            d ^= e >>> 16; g = (g + d)|0; e = (e + f)|0;
-            e ^= f <<  10; h = (h + e)|0; f = (f + g)|0;
-            f ^= g >>>  4; a = (a + f)|0; g = (g + h)|0;
-            g ^= h <<   8; b = (b + g)|0; h = (h + a)|0;
-            h ^= a >>>  9; c = (c + h)|0; a = (a + b)|0;
+                m[i|0] ^= a,
+                m[i|1] ^= b,
+                m[i|2] ^= c,
+                m[i|3] ^= d,
+                m[i|4] ^= e,
+                m[i|5] ^= f,
+                m[i|6] ^= g,
+                m[i|7] ^= h;
+            }
 
-            m[i|0] = a,
-            m[i|1] = b,
-            m[i|2] = c,
-            m[i|3] = d,
-            m[i|4] = e,
-            m[i|5] = f,
-            m[i|6] = g,
-            m[i|7] = h;
+            // mix it again
+            for ( i = 0; i < 256; i += 8 ) {
+                a = (a + m[i|0])|0; b = (b + m[i|1])|0;
+                c = (c + m[i|2])|0; d = (d + m[i|3])|0;
+                e = (e + m[i|4])|0; f = (f + m[i|5])|0;
+                g = (g + m[i|6])|0; h = (h + m[i|7])|0;
+
+                a ^= b <<  11; d = (d + a)|0; b = (b + c)|0;
+                b ^= c >>>  2; e = (e + b)|0; c = (c + d)|0;
+                c ^= d <<   8; f = (f + c)|0; d = (d + e)|0;
+                d ^= e >>> 16; g = (g + d)|0; e = (e + f)|0;
+                e ^= f <<  10; h = (h + e)|0; f = (f + g)|0;
+                f ^= g >>>  4; a = (a + f)|0; g = (g + h)|0;
+                g ^= h <<   8; b = (b + g)|0; h = (h + a)|0;
+                h ^= a >>>  9; c = (c + h)|0; a = (a + b)|0;
+
+                m[i|0] = a,
+                m[i|1] = b,
+                m[i|2] = c,
+                m[i|3] = d,
+                m[i|4] = e,
+                m[i|5] = f,
+                m[i|6] = g,
+                m[i|7] = h;
+            }
         }
 
         // fill in the first set of results
