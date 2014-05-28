bb8b430557c8a0eba9b75998af5c1c873901928c
Artem S Vybornov <vybornov@gmail.com>
2014-05-28 12:10:50 +0400

    Comment on components dependencies
    https://github.com/vibornoff/asmcrypto.js/issues/25#issuecomment-44353551

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-feature');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-clean');
@@ -27,28 +26,101 @@ module.exports = function(grunt) {
 */
                 files: {
                     'asmcrypto.js': [
-                        'src/utils.js',
+                        // 'common'
                         'src/errors.js',
 
-                        'src/aes/aes.asm.js', 'src/aes/aes.js', 'src/aes/aes-cbc.js', 'src/aes/aes-ccm.js', 'src/aes/aes-cfb.js', 'src/aes/exports.js',
+                        // 'utils'
+                        'src/utils.js',
 
-                        'src/sha1/sha1.asm.js', 'src/sha1/sha1.js', 'src/sha1/exports.js',
+                        // 'aes' (depends on 'common', 'utils')
+                        'src/aes/aes.asm.js', 'src/aes/aes.js',
 
-                        'src/sha256/sha256.asm.js', 'src/sha256/sha256.js', 'src/sha256/exports.js',
+                        // 'aes-cbc' (depends on 'aes')
+                        'src/aes/aes-cbc.js',
 
-                        'src/sha512/sha512.asm.js', 'src/sha512/sha512.js', 'src/sha512/exports.js',
+                        // 'aes-ccm' (depends on 'aes')
+                        'src/aes/aes-ccm.js',
 
-                        'src/hmac/hmac.js', 'src/hmac/hmac-sha1.js', 'src/hmac/hmac-sha256.js', 'src/hmac/hmac-sha512.js', 'src/hmac/exports.js',
+                        // 'aes-cfb' (depends on 'aes', off by default)
+                        'src/aes/aes-cfb.js',
 
-                        'src/pbkdf2/pbkdf2.js', 'src/pbkdf2/pbkdf2-hmac-sha1.js', 'src/pbkdf2/pbkdf2-hmac-sha256.js', 'src/pbkdf2/pbkdf2-hmac-sha512.js', 'src/pbkdf2/exports.js',
+                        // 'exports-aes' (depends on 'aes')
+                        'src/aes/exports.js',
 
-                        'src/random/isaac.js', 'src/random/random.js', 'src/random/exports.js',
+                        // 'sha1' (depends on 'common', 'utils')
+                        'src/sha1/sha1.asm.js', 'src/sha1/sha1.js',
 
-                        'src/bignum/bigint.asm.js', 'src/bignum/bignum.js', 'src/bignum/extgcd.js', 'src/bignum/modulus.js', 'src/bignum/prime.js', 'src/bignum/exports.js',
+                        // 'exports-sha1' (depends on 'sha1')
+                        'src/sha1/exports.js',
 
-                        'src/rsa/rsa.js', 'src/rsa/genkey.js', 'src/rsa/pkcs1.js', 'src/rsa/exports.js',
+                        // 'sha256' (depends on 'common', 'utils')
+                        'src/sha256/sha256.asm.js', 'src/sha256/sha256.js',
 
-                        'src/exports.js', 'src/globals.js'
+                        // 'exports-sha256' (depends on 'sha256')
+                        'src/sha256/exports.js',
+
+                        // 'sha512' (depends on 'common', 'utils', off by default)
+                        'src/sha512/sha512.asm.js', 'src/sha512/sha512.js',
+
+                        // 'exports-sha256' (depends on 'sha512', off by default)
+                        'src/sha512/exports.js',
+
+                        // 'hmac' (depends on 'common', 'utils')
+                        'src/hmac/hmac.js',
+
+                        // 'hmac-sha1' (depends on 'hmac', 'sha1')
+                        'src/hmac/hmac-sha1.js',
+
+                        // 'hmac-sha256' (depends on 'hmac', 'sha256')
+                        'src/hmac/hmac-sha256.js',
+
+                        // 'hmac-sha512' (depends on 'hmac', 'sha512', off by default)
+                        'src/hmac/hmac-sha512.js',
+
+                        // 'exports-hmac' (depends on 'hmac')
+                        'src/hmac/exports.js',
+
+                        // 'pbkdf2' (depends on 'common', 'utils')
+                        'src/pbkdf2/pbkdf2.js',
+
+                        // 'pbkdf2-hmac-sha1' (depends on 'pbkdf2', 'hmac-sha1')
+                        'src/pbkdf2/pbkdf2-hmac-sha1.js',
+
+                        // 'pbkdf2-hmac-sha256' (depends on 'pbkdf2', 'hmac-sha256')
+                        'src/pbkdf2/pbkdf2-hmac-sha256.js',
+
+                        // 'pbkdf2-hmac-sha512' (depends on 'pbkdf2', 'hmac-sha512', off by default)
+                        'src/pbkdf2/pbkdf2-hmac-sha512.js',
+
+                        // 'exports-pbkdf2' (depends on 'pbkdf2')
+                        'src/pbkdf2/exports.js',
+
+                        // 'rng' (depends on 'common', 'utils')
+                        'src/random/isaac.js', 'src/random/random.js',
+
+                        // 'exports-rng' (depends on 'rng')
+                        'src/random/exports.js',
+
+                        // 'bn' (depends on 'common', 'rng')
+                        'src/bignum/bigint.asm.js', 'src/bignum/bignum.js', 'src/bignum/extgcd.js', 'src/bignum/modulus.js', 'src/bignum/prime.js',
+
+                        // 'exports-bn' (depends on 'bn')
+                        'src/bignum/exports.js',
+
+                        // 'rsa' (depends on 'bn', 'rng')
+                        'src/rsa/rsa.js', 'src/rsa/genkey.js',
+
+                        // 'rsa-pkcs1' (depends on 'rsa')
+                        'src/rsa/pkcs1.js',
+
+                        // 'exports-rsa' (depends on 'rsa')
+                        'src/rsa/exports.js',
+
+                        // 'exports' (depends on 'utils')
+                        'src/exports.js',
+
+                        // 'globals' (depends on 'common')
+                        'src/globals.js'
                     ]
                 }
             }
