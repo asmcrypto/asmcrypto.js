1fd8107bd5de666a5fb0e076f1c237939f31e87f
Artem S Vybornov <vybornov@gmail.com>
2014-06-08 02:39:19 +0400

    Typofix

--- src/random/exports.js
+++ src/random/exports.js
@@ -2,7 +2,7 @@ exports.random = Random_getNumber;
 
 exports.random.seed = Random_seed;
 
-Object.defineProperty( exports.random, 'alloWeak', {
+Object.defineProperty( exports.random, 'allowWeak', {
     get: function () { return _random_allow_weak; },
     set: function ( a ) { _random_allow_weak = a; }
 });
@@ -11,7 +11,7 @@ exports.getRandomValues = Random_getValues;
 
 exports.getRandomValues.seed = Random_seed;
 
-Object.defineProperty( exports.getRandomValues, 'alloWeak', {
+Object.defineProperty( exports.getRandomValues, 'allowWeak', {
     get: function () { return _random_allow_weak; },
     set: function ( a ) { _random_allow_weak = a; }
 });
