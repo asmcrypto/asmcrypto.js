30264c1289d34246c9c962945ad1912f86fd8d74
Artem S Vybornov <vybornov@gmail.com>
2014-06-14 21:15:18 +0400

    Fix RSA-PKCS1 exports

--- src/rsa/exports.js
+++ src/rsa/exports.js
@@ -41,151 +41,166 @@ if ( typeof RSA_RAW !== 'undefined' ) {
 }
 
 /**
- * RSA-OAEP-SHA1 exports
- * RSA-PSS-SHA1 exports
+ * RSA-OAEP-* exports
  */
-if ( typeof get_sha1_instance !== 'undefined' )
+if ( typeof RSA_OAEP !== 'undefined' )
 {
-    function rsa_oaep_sha1_encrypt_bytes ( data, key, label ) {
-        if ( data === undefined ) throw new SyntaxError("data required");
-        if ( key === undefined ) throw new SyntaxError("key required");
-        return (new RSA_OAEP({ hash: get_sha1_instance(), key: key, label: label })).encrypt(data).result;
-    }
-
-    function rsa_oaep_sha1_decrypt_bytes ( data, key, label ) {
-        if ( data === undefined ) throw new SyntaxError("data required");
-        if ( key === undefined ) throw new SyntaxError("key required");
-        return (new RSA_OAEP({ hash: get_sha1_instance(), key: key, label: label })).decrypt(data).result;
-    }
-
-    exports.RSA_OAEP_SHA1 = {
-        encrypt: rsa_oaep_sha1_encrypt_bytes,
-        decrypt: rsa_oaep_sha1_decrypt_bytes
-    };
-
-    function rsa_pss_sha1_sign_bytes ( data, key, slen ) {
-        if ( data === undefined ) throw new SyntaxError("data required");
-        if ( key === undefined ) throw new SyntaxError("key required");
-        return (new RSA_PSS({ hash: get_sha1_instance(), key: key, saltLength: slen })).sign(data).result;
-    }
-
-    function rsa_pss_sha1_verify_bytes ( signature, data, key, slen ) {
-        if ( signature === undefined ) throw new SyntaxError("signature required");
-        if ( data === undefined ) throw new SyntaxError("data required");
-        if ( key === undefined ) throw new SyntaxError("key required");
-        try {
-            (new RSA_PSS({ hash: get_sha1_instance(), key: key, saltLength: slen })).verify(signature, data);
-            return true;
+    // RSA-OAEP-SHA1
+    if ( typeof get_sha1_instance !== 'undefined' )
+    {
+        function rsa_oaep_sha1_encrypt_bytes ( data, key, label ) {
+            if ( data === undefined ) throw new SyntaxError("data required");
+            if ( key === undefined ) throw new SyntaxError("key required");
+            return (new RSA_OAEP({ hash: get_sha1_instance(), key: key, label: label })).encrypt(data).result;
         }
-        catch ( e ) {
-            if ( !( e instanceof SecurityError ) )
-                throw e;
+
+        function rsa_oaep_sha1_decrypt_bytes ( data, key, label ) {
+            if ( data === undefined ) throw new SyntaxError("data required");
+            if ( key === undefined ) throw new SyntaxError("key required");
+            return (new RSA_OAEP({ hash: get_sha1_instance(), key: key, label: label })).decrypt(data).result;
         }
-        return false;
+
+        exports.RSA_OAEP_SHA1 = {
+            encrypt: rsa_oaep_sha1_encrypt_bytes,
+            decrypt: rsa_oaep_sha1_decrypt_bytes
+        };
     }
 
-    exports.RSA_PSS_SHA1 = {
-        sign: rsa_pss_sha1_sign_bytes,
-        verify: rsa_pss_sha1_verify_bytes
-    };
+    // RSA-OAEP-SHA256
+    if ( typeof get_sha256_instance !== 'undefined' )
+    {
+        function rsa_oaep_sha256_encrypt_bytes ( data, key, label ) {
+            if ( data === undefined ) throw new SyntaxError("data required");
+            if ( key === undefined ) throw new SyntaxError("key required");
+            return (new RSA_OAEP({ hash: get_sha256_instance(), key: key, label: label })).encrypt(data).result;
+        }
+
+        function rsa_oaep_sha256_decrypt_bytes ( data, key, label ) {
+            if ( data === undefined ) throw new SyntaxError("data required");
+            if ( key === undefined ) throw new SyntaxError("key required");
+            return (new RSA_OAEP({ hash: get_sha256_instance(), key: key, label: label })).decrypt(data).result;
+        }
+
+        exports.RSA_OAEP_SHA256 = {
+            encrypt: rsa_oaep_sha256_encrypt_bytes,
+            decrypt: rsa_oaep_sha256_decrypt_bytes
+        };
+    }
+
+    // RSA-OAEP-SHA512
+    if ( typeof get_sha512_instance !== 'undefined' )
+    {
+        function rsa_oaep_sha512_encrypt_bytes ( data, key, label ) {
+            if ( data === undefined ) throw new SyntaxError("data required");
+            if ( key === undefined ) throw new SyntaxError("key required");
+            return (new RSA_OAEP({ hash: get_sha512_instance(), key: key, label: label })).encrypt(data).result;
+        }
+
+        function rsa_oaep_sha512_decrypt_bytes ( data, key, label ) {
+            if ( data === undefined ) throw new SyntaxError("data required");
+            if ( key === undefined ) throw new SyntaxError("key required");
+            return (new RSA_OAEP({ hash: get_sha512_instance(), key: key, label: label })).decrypt(data).result;
+        }
+
+        exports.RSA_OAEP_SHA512 = {
+            encrypt: rsa_oaep_sha512_encrypt_bytes,
+            decrypt: rsa_oaep_sha512_decrypt_bytes
+        };
+    }
 }
 
 /**
- * RSA-OAEP-SHA256 exports
- * RSA-PSS-SHA256 exports
+ * RSA-PSS-* exports
  */
-if ( typeof get_sha256_instance !== 'undefined' )
+if ( typeof RSA_PSS !== 'undefined' )
 {
-    function rsa_oaep_sha256_encrypt_bytes ( data, key, label ) {
-        if ( data === undefined ) throw new SyntaxError("data required");
-        if ( key === undefined ) throw new SyntaxError("key required");
-        return (new RSA_OAEP({ hash: get_sha256_instance(), key: key, label: label })).encrypt(data).result;
-    }
-
-    function rsa_oaep_sha256_decrypt_bytes ( data, key, label ) {
-        if ( data === undefined ) throw new SyntaxError("data required");
-        if ( key === undefined ) throw new SyntaxError("key required");
-        return (new RSA_OAEP({ hash: get_sha256_instance(), key: key, label: label })).decrypt(data).result;
-    }
-
-    exports.RSA_OAEP_SHA256 = {
-        encrypt: rsa_oaep_sha256_encrypt_bytes,
-        decrypt: rsa_oaep_sha256_decrypt_bytes
-    };
-
-    function rsa_pss_sha256_sign_bytes ( data, key, slen ) {
-        if ( data === undefined ) throw new SyntaxError("data required");
-        if ( key === undefined ) throw new SyntaxError("key required");
-        return (new RSA_PSS({ hash: get_sha256_instance(), key: key, saltLength: slen })).sign(data).result;
-    }
-
-    function rsa_pss_sha256_verify_bytes ( signature, data, key, slen ) {
-        if ( signature === undefined ) throw new SyntaxError("signature required");
-        if ( data === undefined ) throw new SyntaxError("data required");
-        if ( key === undefined ) throw new SyntaxError("key required");
-        try {
-            (new RSA_PSS({ hash: get_sha256_instance(), key: key, saltLength: slen })).verify(signature, data);
-            return true;
+    // RSA-PSS-SHA1
+    if ( typeof get_sha1_instance !== 'undefined' )
+    {
+        function rsa_pss_sha1_sign_bytes ( data, key, slen ) {
+            if ( data === undefined ) throw new SyntaxError("data required");
+            if ( key === undefined ) throw new SyntaxError("key required");
+            return (new RSA_PSS({ hash: get_sha1_instance(), key: key, saltLength: slen })).sign(data).result;
         }
-        catch ( e ) {
-            if ( !( e instanceof SecurityError ) )
-                throw e;
+
+        function rsa_pss_sha1_verify_bytes ( signature, data, key, slen ) {
+            if ( signature === undefined ) throw new SyntaxError("signature required");
+            if ( data === undefined ) throw new SyntaxError("data required");
+            if ( key === undefined ) throw new SyntaxError("key required");
+            try {
+                (new RSA_PSS({ hash: get_sha1_instance(), key: key, saltLength: slen })).verify(signature, data);
+                return true;
+            }
+            catch ( e ) {
+                if ( !( e instanceof SecurityError ) )
+                    throw e;
+            }
+            return false;
         }
-        return false;
+
+        exports.RSA_PSS_SHA1 = {
+            sign: rsa_pss_sha1_sign_bytes,
+            verify: rsa_pss_sha1_verify_bytes
+        };
     }
 
-    exports.RSA_PSS_SHA256 = {
-        sign: rsa_pss_sha256_sign_bytes,
-        verify: rsa_pss_sha256_verify_bytes
-    };
-}
-
-/**
- * RSA-OAEP-SHA512 exports
- * RSA-PSS-SHA512 exports
- */
-if ( typeof get_sha512_instance !== 'undefined' )
-{
-    function rsa_oaep_sha512_encrypt_bytes ( data, key, label ) {
-        if ( data === undefined ) throw new SyntaxError("data required");
-        if ( key === undefined ) throw new SyntaxError("key required");
-        return (new RSA_OAEP({ hash: get_sha512_instance(), key: key, label: label })).encrypt(data).result;
-    }
-
-    function rsa_oaep_sha512_decrypt_bytes ( data, key, label ) {
-        if ( data === undefined ) throw new SyntaxError("data required");
-        if ( key === undefined ) throw new SyntaxError("key required");
-        return (new RSA_OAEP({ hash: get_sha512_instance(), key: key, label: label })).decrypt(data).result;
-    }
-
-    exports.RSA_OAEP_SHA512 = {
-        encrypt: rsa_oaep_sha512_encrypt_bytes,
-        decrypt: rsa_oaep_sha512_decrypt_bytes
-    };
-
-    function rsa_pss_sha512_sign_bytes ( data, key, slen ) {
-        if ( data === undefined ) throw new SyntaxError("data required");
-        if ( key === undefined ) throw new SyntaxError("key required");
-        return (new RSA_PSS({ hash: get_sha512_instance(), key: key, saltLength: slen })).sign(data).result;
-    }
-
-    function rsa_pss_sha512_verify_bytes ( signature, data, key, slen ) {
-        if ( signature === undefined ) throw new SyntaxError("signature required");
-        if ( data === undefined ) throw new SyntaxError("data required");
-        if ( key === undefined ) throw new SyntaxError("key required");
-        try {
-            (new RSA_PSS({ hash: get_sha512_instance(), key: key, saltLength: slen })).verify(signature, data);
-            return true;
+    // RSA-PSS-SHA256
+    if ( typeof get_sha256_instance !== 'undefined' )
+    {
+        function rsa_pss_sha256_sign_bytes ( data, key, slen ) {
+            if ( data === undefined ) throw new SyntaxError("data required");
+            if ( key === undefined ) throw new SyntaxError("key required");
+            return (new RSA_PSS({ hash: get_sha256_instance(), key: key, saltLength: slen })).sign(data).result;
         }
-        catch ( e ) {
-            if ( !( e instanceof SecurityError ) )
-                throw e;
+
+        function rsa_pss_sha256_verify_bytes ( signature, data, key, slen ) {
+            if ( signature === undefined ) throw new SyntaxError("signature required");
+            if ( data === undefined ) throw new SyntaxError("data required");
+            if ( key === undefined ) throw new SyntaxError("key required");
+            try {
+                (new RSA_PSS({ hash: get_sha256_instance(), key: key, saltLength: slen })).verify(signature, data);
+                return true;
+            }
+            catch ( e ) {
+                if ( !( e instanceof SecurityError ) )
+                    throw e;
+            }
+            return false;
         }
-        return false;
+
+        exports.RSA_PSS_SHA256 = {
+            sign: rsa_pss_sha256_sign_bytes,
+            verify: rsa_pss_sha256_verify_bytes
+        };
     }
 
-    exports.RSA_PSS_SHA512 = {
-        sign: rsa_pss_sha512_sign_bytes,
-        verify: rsa_pss_sha512_verify_bytes
-    };
+    // RSA-PSS-SHA512
+    if ( typeof get_sha512_instance !== 'undefined' )
+    {
+        function rsa_pss_sha512_sign_bytes ( data, key, slen ) {
+            if ( data === undefined ) throw new SyntaxError("data required");
+            if ( key === undefined ) throw new SyntaxError("key required");
+            return (new RSA_PSS({ hash: get_sha512_instance(), key: key, saltLength: slen })).sign(data).result;
+        }
+
+        function rsa_pss_sha512_verify_bytes ( signature, data, key, slen ) {
+            if ( signature === undefined ) throw new SyntaxError("signature required");
+            if ( data === undefined ) throw new SyntaxError("data required");
+            if ( key === undefined ) throw new SyntaxError("key required");
+            try {
+                (new RSA_PSS({ hash: get_sha512_instance(), key: key, saltLength: slen })).verify(signature, data);
+                return true;
+            }
+            catch ( e ) {
+                if ( !( e instanceof SecurityError ) )
+                    throw e;
+            }
+            return false;
+        }
+
+        exports.RSA_PSS_SHA512 = {
+            sign: rsa_pss_sha512_sign_bytes,
+            verify: rsa_pss_sha512_verify_bytes
+        };
+    }
 }
