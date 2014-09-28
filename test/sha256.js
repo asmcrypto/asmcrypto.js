f83559e1f8a9e2534c1ab0c5f3385ff2f94e2b10
Ian Coleman <coleman.ian@gmail.com>
2014-09-28 11:20:44 +1000

    sha256 tests utf8 strings

--- test/sha256.js
+++ test/sha256.js
@@ -12,7 +12,8 @@ if ( typeof asmCrypto.SHA256 !== 'undefined' )
         [ '6819d915c73f4d1e77e4e1b52d1fa0f9cf9beaead3939f15874bd988e2a23630', 'SHA256 is considered to be safe' ],
         [ '248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1', 'abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq' ],
         [ 'f08a78cbbaee082b052ae0708f32fa1e50c5c421aa772ba5dbb406a2ea6be342', 'For this sample, this 63-byte string will be used as input data' ],
-        [ 'ab64eff7e88e2e46165e29f2bce41826bd4c7b3552f6b382a9e7d3af47c245f8', 'This is exactly 64 bytes long, not counting the terminating byte' ]
+        [ 'ab64eff7e88e2e46165e29f2bce41826bd4c7b3552f6b382a9e7d3af47c245f8', 'This is exactly 64 bytes long, not counting the terminating byte' ],
+        [ '11b4099132815e057ef4db0350ad5eaf0e0e179009b52421c2fd761928edd587', 'This string contains UTF8 characters - Ͼ' ],
     ];
 
     test( "asmCrypto.SHA256.hex", function () {
@@ -37,7 +38,8 @@ if ( typeof asmCrypto.HMAC_SHA256 !== 'undefined' )
     var hmac_sha256_vectors = [
         [ 'b613679a0814d9ec772f95d778c35fc5ff1697c493715653c6c712144292c5ad', '', '' ],
         [ 'f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8', 'key', 'The quick brown fox jumps over the lazy dog' ],
-        [ 'b54d57e9b21940b6496b58d5ac120eda9f1637788b5df058928637f2eca40cd9', 'MyVeeeeeeeeeeeeeryVeeeeeeeeeeeeeryVeeeeeeeeeeeeeryLoooooooooongPassword', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' ]
+        [ 'b54d57e9b21940b6496b58d5ac120eda9f1637788b5df058928637f2eca40cd9', 'MyVeeeeeeeeeeeeeryVeeeeeeeeeeeeeryVeeeeeeeeeeeeeryLoooooooooongPassword', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' ],
+        [ '8608ee9fb1c93ce173d1d188bcb669a5c3e5154f352f106c4cee31fe61622f22', 'This data contains UTF8 characters - Ͼ', 'This password contains UTF8 characters - Ͼ' ],
     ];
 
     test( "asmCrypto.HMAC_SHA256.hex", function () {
@@ -65,7 +67,8 @@ if ( typeof asmCrypto.PBKDF2_HMAC_SHA256 !== 'undefined' )
         [ 'c5e478d59288c841aa530db6845c4c8d962893a001ce4e11a4963873aa98134a', 'password', 'salt', 4096, 32 ],
         [ '348c89dbcbd32b2f32d814b8116e84cf2b17347ebc1800181c4e2a1fb8dd53e1c635518c7dac47e9', 'passwordPASSWORDpassword', 'saltSALTsaltSALTsaltSALTsaltSALTsalt', 4096, 40 ],
         [ '89b69d0516f829893c696226650a8687', "pass\0word", "sa\0lt", 4096, 16 ],
-        [ 'cdc8b1780ca68aba97f1f729c9d281719702eb4b308d7d87409817e60188be0d', 'MyVeeeeeeeeeeeeeryVeeeeeeeeeeeeeryVeeeeeeeeeeeeeryLoooooooooongPassword', 'MyVeeeeeeeeeeeeeryVeeeeeeeeeeeeeryVeeeeeeeeeeeeeryLoooooooooongPassword', 4096, 32 ]
+        [ 'cdc8b1780ca68aba97f1f729c9d281719702eb4b308d7d87409817e60188be0d', 'MyVeeeeeeeeeeeeeryVeeeeeeeeeeeeeryVeeeeeeeeeeeeeryLoooooooooongPassword', 'MyVeeeeeeeeeeeeeryVeeeeeeeeeeeeeryVeeeeeeeeeeeeeryLoooooooooongPassword', 4096, 32 ],
+        [ 'cfa8aa80136fbac4bf51ceca36745268d20953d189bf5a74852e459716737a8b', 'This data contains UTF8 characters - Ͼ', 'This password contains UTF8 characters - Ͼ', 1, 32 ],
     ];
 
     test( "asmCrypto.PBKDF2_HMAC_SHA256.hex", function () {
