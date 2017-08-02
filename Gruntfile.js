d4301d1b47c2545c341a63da067d0dce12e7254c
Mihaly Lengyel <mihaly.lengyel@tresorit.com>
2017-08-02 16:26:17 +0200

    Updated Gruntfile to use rollup and added dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,337 +17,6 @@ var defaults = [
     'origin',
 ];
 
-// Map each of the modules and their deps
-// Topo-sorted
-var modules = [
-    {
-        name: 'common',
-        files: [ 'src/errors.js' ]
-    },
-    {
-        name: 'utils',
-        files: [ 'src/utils.js' ],
-        implies: [ 'exports' ]
-    },
-    {
-        name: 'origin',
-        files: [ 'src/origin.js' ],
-    },
-    {
-        name: 'exports',
-        files: [ 'src/exports.js' ],
-        depends: [ 'utils' ]
-    },
-    {
-        name: 'globals',
-        files: [ 'src/globals.js' ],
-        depends: [ 'common' ]
-    },
-    {
-        name: 'aes',
-        files: [ 'src/aes/aes.asm.js', 'src/aes/aes.js' ],
-        depends: [ 'common', 'utils' ]
-    },
-    {
-        name: 'aes-ecb',
-        files: [ 'src/aes/ecb/ecb.js' ],
-        depends: [ 'aes' ],
-        implies: [ 'aes-exports', 'aes-ecb-exports' ]
-    },
-    {
-        name: 'aes-cbc',
-        files: [ 'src/aes/cbc/cbc.js' ],
-        depends: [ 'aes' ],
-        implies: [ 'aes-exports', 'aes-cbc-exports' ]
-    },
-    {
-        name: 'aes-cfb',
-        files: [ 'src/aes/cfb/cfb.js' ],
-        depends: [ 'aes' ],
-        implies: [ 'aes-exports', 'aes-cfb-exports' ]
-    },
-    {
-        name: 'aes-ofb',
-        files: [ 'src/aes/ofb/ofb.js' ],
-        depends: [ 'aes' ],
-        implies: [ 'aes-exports', 'aes-ofb-exports' ]
-    },
-    {
-        name: 'aes-ctr',
-        files: [ 'src/aes/ctr/ctr.js' ],
-        depends: [ 'aes' ],
-        implies: [ 'aes-exports', 'aes-ctr-exports' ]
-    },
-    {
-        name: 'aes-ccm',
-        files: [ 'src/aes/ccm/ccm.js' ],
-        depends: [ 'aes', 'aes-ctr' ],
-        implies: [ 'aes-exports', 'aes-ccm-exports' ]
-    },
-    {
-        name: 'aes-gcm',
-        files: [ 'src/aes/gcm/gcm.js' ],
-        depends: [ 'aes', 'aes-ctr' ],
-        implies: [ 'aes-exports', 'aes-gcm-exports' ]
-    },
-    {
-        name: 'aes-exports',
-        files: [ 'src/aes/exports.js' ],
-        depends: [ 'aes' ]
-    },
-    {
-        name: 'aes-ecb-exports',
-        files: [ 'src/aes/ecb/exports.js' ],
-        depends: [ 'aes-ecb', 'aes-exports' ]
-    },
-    {
-        name: 'aes-cbc-exports',
-        files: [ 'src/aes/cbc/exports.js' ],
-        depends: [ 'aes-cbc', 'aes-exports' ]
-    },
-    {
-        name: 'aes-cfb-exports',
-        files: [ 'src/aes/cfb/exports.js' ],
-        depends: [ 'aes-cfb', 'aes-exports' ]
-    },
-    {
-        name: 'aes-ofb-exports',
-        files: [ 'src/aes/ofb/exports.js' ],
-        depends: [ 'aes-ofb', 'aes-exports' ]
-    },
-    {
-        name: 'aes-ctr-exports',
-        files: [ 'src/aes/ctr/exports.js' ],
-        depends: [ 'aes-ctr', 'aes-exports' ]
-    },
-    {
-        name: 'aes-ccm-exports',
-        files: [ 'src/aes/ccm/exports.js' ],
-        depends: [ 'aes-ccm', 'aes-exports' ]
-    },
-    {
-        name: 'aes-gcm-exports',
-        files: [ 'src/aes/gcm/exports.js' ],
-        depends: [ 'aes-gcm', 'aes-exports' ]
-    },
-    {
-        name: 'hash',
-        files: [ 'src/hash/hash.js' ],
-        depends: [ 'common', 'utils' ]
-    },
-    {
-        name: 'sha1',
-        files: [ 'src/hash/sha1/sha1.asm.js', 'src/hash/sha1/sha1.js' ],
-        depends: [ 'common', 'hash', 'utils' ],
-        implies: [ 'sha1-exports' ]
-    },
-    {
-        name: 'sha1-exports',
-        files: [ 'src/hash/sha1/exports.js' ],
-        depends: [ 'sha1' ]
-    },
-    {
-        name: 'sha256',
-        files: [ 'src/hash/sha256/sha256.asm.js', 'src/hash/sha256/sha256.js' ],
-        depends: [ 'common', 'hash', 'utils' ],
-        implies: [ 'sha256-exports' ]
-    },
-    {
-        name: 'sha256-exports',
-        files: [ 'src/hash/sha256/exports.js' ],
-        depends: [ 'sha256' ],
-    },
-    {
-        name: 'sha512',
-        files: [ 'src/hash/sha512/sha512.asm.js', 'src/hash/sha512/sha512.js' ],
-        depends: [ 'common', 'hash', 'utils' ],
-        implies: [ 'sha512-exports' ]
-    },
-    {
-        name: 'sha512-exports',
-        files: [ 'src/hash/sha512/exports.js' ],
-        depends: [ 'sha512' ]
-    },
-    {
-        name: 'hmac',
-        files: [ 'src/hmac/hmac.js' ],
-        depends: [ 'common', 'utils' ]
-    },
-    {
-        name: 'hmac-sha1',
-        files: [ 'src/hmac/hmac-sha1.js' ],
-        depends: [ 'hmac', 'sha1' ],
-        implies: [ 'hmac-sha1-exports' ]
-    },
-    {
-        name: 'hmac-sha256',
-        files: [ 'src/hmac/hmac-sha256.js' ],
-        depends: [ 'hmac', 'sha256' ],
-        implies: [ 'hmac-sha256-exports' ]
-    },
-    {
-        name: 'hmac-sha512',
-        files: [ 'src/hmac/hmac-sha512.js' ],
-        depends: [ 'hmac', 'sha512' ],
-        implies: [ 'hmac-sha512-exports' ]
-    },
-    {
-        name: 'hmac-sha1-exports',
-        files: [ 'src/hmac/exports-hmac-sha1.js' ],
-        depends: [ 'hmac-sha1' ]
-    },
-    {
-        name: 'hmac-sha256-exports',
-        files: [ 'src/hmac/exports-hmac-sha256.js' ],
-        depends: [ 'hmac-sha256' ]
-    },
-    {
-        name: 'hmac-sha512-exports',
-        files: [ 'src/hmac/exports-hmac-sha512.js' ],
-        depends: [ 'hmac-sha512' ]
-    },
-    {
-        name: 'pbkdf2',
-        files: [ 'src/pbkdf2/pbkdf2.js' ],
-        depends: [ 'common', 'utils' ]
-    },
-    {
-        name: 'pbkdf2-hmac-sha1',
-        files: [ 'src/pbkdf2/pbkdf2-hmac-sha1.js' ],
-        depends: [ 'pbkdf2', 'hmac-sha1' ],
-        implies: [ 'pbkdf2-hmac-sha1-exports' ]
-    },
-    {
-        name: 'pbkdf2-hmac-sha256',
-        files: [ 'src/pbkdf2/pbkdf2-hmac-sha256.js' ],
-        depends: [ 'pbkdf2', 'hmac-sha256' ],
-        implies: [ 'pbkdf2-hmac-sha256-exports' ]
-    },
-    {
-        name: 'pbkdf2-hmac-sha512',
-        files: [ 'src/pbkdf2/pbkdf2-hmac-sha512.js' ],
-        depends: [ 'pbkdf2', 'hmac-sha512' ],
-        implies: [ 'pbkdf2-hmac-sha512-exports' ]
-    },
-    {
-        name: 'pbkdf2-hmac-sha1-exports',
-        files: [ 'src/pbkdf2/exports-pbkdf2-hmac-sha1.js' ],
-        depends: [ 'pbkdf2' ]
-    },
-    {
-        name: 'pbkdf2-hmac-sha256-exports',
-        files: [ 'src/pbkdf2/exports-pbkdf2-hmac-sha256.js' ],
-        depends: [ 'pbkdf2' ]
-    },
-    {
-        name: 'pbkdf2-hmac-sha512-exports',
-        files: [ 'src/pbkdf2/exports-pbkdf2-hmac-sha512.js' ],
-        depends: [ 'pbkdf2' ]
-    },
-    {
-        name: 'rng',
-        files: [ 'src/random/isaac.js', 'src/random/random.js' ],
-        depends: [ 'common', 'utils', 'pbkdf2-hmac-sha256' ],
-        implies: [ 'rng-exports' ]
-    },
-    {
-        name: 'rng-exports',
-        files: [ 'src/random/exports.js' ],
-        depends: [ 'rng' ]
-    },
-    {
-        name: 'rng-globals',
-        files: [ 'src/random/globals.js' ],
-        depends: [ 'rng' ]
-    },
-    {
-        name: 'bn',
-        files: [ 'src/bignum/bigint.asm.js', 'src/bignum/bignum.js', 'src/bignum/extgcd.js', 'src/bignum/modulus.js', 'src/bignum/prime.js' ],
-        depends: [ 'common', 'rng' ],
-        implies: [ 'bn-exports' ]
-    },
-    {
-        name: 'bn-exports',
-        files: [ 'src/bignum/exports.js' ],
-        depends: [ 'bn' ]
-    },
-    {
-        name: 'rsa',
-        files: [ 'src/rsa/rsa.js', 'src/rsa/genkey.js' ],
-        depends: [ 'bn', 'rng' ],
-        implies: [ 'rsa-keygen-exports' ]
-    },
-    {
-        name: 'rsa-raw',
-        files: [ 'src/rsa/raw.js' ],
-        depends: [ 'rsa' ],
-        implies: [ 'rsa-keygen-exports', 'rsa-raw-exports' ]
-    },
-    {
-        name: 'rsa-pkcs1',
-        files: [ 'src/rsa/pkcs1.js' ],
-        depends: [ 'rsa' ],
-        implies: [ 'rsa-keygen-exports', 'rsa-oaep-sha1-exports', 'rsa-oaep-sha256-exports', 'rsa-oaep-sha512-exports',
-                                         'rsa-pss-sha1-exports',  'rsa-pss-sha256-exports',  'rsa-pss-sha512-exports' ,
-                                         'rsa-pkcs1-v1_5-sha1-exports',  'rsa-pkcs1-v1_5-sha256-exports', 'rsa-pkcs1-v1_5-sha512-exports' ]
-    },
-    {
-        name: 'rsa-keygen-exports',
-        files: [ 'src/rsa/exports-keygen.js' ],
-        depends: [ 'rsa' ]
-    },
-    {
-        name: 'rsa-raw-exports',
-        files: [ 'src/rsa/exports-raw.js' ],
-        depends: [ 'rsa-raw' ]
-    },
-    {
-        name: 'rsa-oaep-sha1-exports',
-        files: [ 'src/rsa/exports-oaep-sha1.js' ],
-        depends: [ 'rsa-pkcs1', 'sha1' ]
-    },
-    {
-        name: 'rsa-oaep-sha256-exports',
-        files: [ 'src/rsa/exports-oaep-sha256.js' ],
-        depends: [ 'rsa-pkcs1', 'sha256' ]
-    },
-    {
-        name: 'rsa-oaep-sha512-exports',
-        files: [ 'src/rsa/exports-oaep-sha512.js' ],
-        depends: [ 'rsa-pkcs1', 'sha512' ]
-    },
-    {
-        name: 'rsa-pss-sha1-exports',
-        files: [ 'src/rsa/exports-pss-sha1.js' ],
-        depends: [ 'rsa-pkcs1', 'sha1' ]
-    },
-    {
-        name: 'rsa-pss-sha256-exports',
-        files: [ 'src/rsa/exports-pss-sha256.js' ],
-        depends: [ 'rsa-pkcs1', 'sha256' ]
-    },
-    {
-        name: 'rsa-pss-sha512-exports',
-        files: [ 'src/rsa/exports-pss-sha512.js' ],
-        depends: [ 'rsa-pkcs1', 'sha512' ]
-    },
-    {
-        name: 'rsa-pkcs1-v1_5-sha1-exports',
-        files: [ 'src/rsa/exports-pkcs1-v1_5-sha1.js' ],
-        depends: [ 'rsa-pkcs1', 'sha1' ]
-    },
-    {
-        name: 'rsa-pkcs1-v1_5-sha256-exports',
-        files: [ 'src/rsa/exports-pkcs1-v1_5-sha256.js' ],
-        depends: [ 'rsa-pkcs1', 'sha256' ]
-    },
-    {
-        name: 'rsa-pkcs1-v1_5-sha512-exports',
-        files: [ 'src/rsa/exports-pkcs1-v1_5-sha512.js' ],
-        depends: [ 'rsa-pkcs1', 'sha512' ]
-    },
-];
-
 // Supported browsers
 var browsers = [
     // Latest browsers
@@ -384,7 +53,7 @@ var browsers = [
 ];
 
 // Grunt setup
-module.exports = function ( grunt ) {
+module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-qunit');
@@ -393,98 +62,42 @@ module.exports = function ( grunt ) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-saucelabs');
     grunt.loadNpmTasks('grunt-jsdoc');
-
-    function sources () {
-        // Get the list of modules split by commas
-        var includeModules = {};
-        ( grunt.option('with') || process.env.WITH || '' ).split(',')
-            .map( function ( moduleName ) { return moduleName.trim() } )
-            .filter( function ( moduleName ) { return moduleName.length > 0 } )
-            .forEach( function ( moduleName ) { includeModules[ moduleName ] = true } );
-
-        // Check for keyword "ALL"
-        if ( includeModules.ALL ) {
-            includeModules = {};
-            modules.forEach( function ( module ) { includeModules[ module.name ] = true } );
-        }
-
-        // No modules specified, put defaults here
-        if ( Object.keys( includeModules ).length === 0 )
-            defaults.forEach( function ( moduleName ) { includeModules[moduleName] = true } );
-
-        // Trace build configuration
-        var traceModules = modules.filter( function ( module ) { return includeModules[module.name] } )
-                                  .map( function ( module ) { return module.name } );
-        grunt.log.writeln( "Building modules: " + traceModules.join(", ") );
-
-        // Recurse into dependancy tree
-        function traverseDependancies ( moduleName, includeModules ) {
-            if ( includeModules[moduleName] )
-                return;
-
-            var result = modules.filter( function ( module ) { return module.name === moduleName } );
-            if ( !result || result.length === 0 )
-                grunt.fail.fatal( "An unknown module '" + moduleName + "' specified" );
-
-            includeModules[ result[0].name ] = true;
-
-            // Get the deps and call recursively
-            var depends = result[0].depends || [];
-
-            depends.forEach( function ( dependsName ) { traverseDependancies( dependsName, includeModules ) } );
-        }
-
-        // Loop each of the specified module names
-        var deepIncludeModules = {};
-        Object.keys(includeModules).forEach( function ( moduleName ) { traverseDependancies( moduleName, deepIncludeModules ) } );
-
-        // Insert implied modules only when theirs dependancies are there
-        var impliedModules = {};
-        for ( var i = 0; i < modules.length; i++ ) {
-            if ( !includeModules[ modules[i].name ] || !modules[i].implies ) continue;
-            modules[i].implies.forEach( function ( moduleName ) { impliedModules[moduleName] = true } );
-        }
-        for ( var i = 0; i < modules.length; i++ ) {
-            if ( !modules[i].depends ) continue;
-            modules[i].depends.forEach( function ( moduleName ) { if ( !deepIncludeModules[moduleName] ) delete impliedModules[ modules[i].name ] } );
-            if ( !impliedModules[ modules[i].name ] ) continue;
-            deepIncludeModules[ modules[i].name ] = true;
-        }
-        Object.keys(impliedModules).forEach( function ( moduleName ) { deepIncludeModules[moduleName] = true } );
-
-        // Hold the array of files as specified by the modules
-        var sourceFiles = [];
-        for ( var i = 0; i < modules.length; i++ ) {
-            if ( !deepIncludeModules[ modules[i].name ] ) continue;
-            sourceFiles = sourceFiles.concat(modules[i].files);
-        }
-
-//        grunt.log.writeln( "Building files:\n" + sourceFiles.join("\n") );
-
-        grunt.config( 'sources.files', sourceFiles );
-
-        return true;
-    }
+    grunt.loadNpmTasks('grunt-rollup');
 
     // Finally, configure
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        concat: {
+        rollup: {
             options: {
-                banner: "/*! asmCrypto<%= pkg.version && ' v'+pkg.version %>, (c) 2013 <%= pkg.author.name %>, opensource.org/licenses/<%= pkg.license %> */\n"
-                      + "(function ( exports, global ) {\n\n",
-                footer: "\n\n'function'==typeof define&&define.amd?define([],function(){return exports}):"
-                      + "'object'==typeof module&&module.exports?module.exports=exports:global.asmCrypto=exports;"
-                      + "\n\nreturn exports;\n})( {}, this );",
+                format: 'umd',
                 sourceMap: true,
-                sourceMapStyle: 'link'
+                banner: "/*! asmCrypto<%= pkg.version && ' v'+pkg.version %>, (c) 2013 <%= pkg.author.name %>, opensource.org/licenses/<%= pkg.license %> */",
+            },
+            default: {
+                options: {
+                    moduleName: 'asmCrypto',
+                },
+                files: {
+                    'asmcrypto.js': './src/entry-default.js'
+                }
             },
             all: {
+                options: {
+                    moduleName: 'asmCrypto',
+                },
                 files: {
-                    'asmcrypto.js': '<%= sources.files %>'
+                    'asmcrypto.js': './src/entry-export_all.js'
                 }
-            }
+            },
+            test_AES_ASM: {
+                options: {
+                    moduleName: 'AES_asm',
+                },
+                files: {
+                    'test/aes.asm.js': './src/aes/aes.asm.js'
+                }
+            },
         },
 
         uglify: {
@@ -506,7 +119,7 @@ module.exports = function ( grunt ) {
 
         jsdoc: {
             all: {
-                src: [ 'src/**/*.js', 'README.md' ],
+                src: ['src/**/*.js', 'README.md'],
                 options: {
                     destination: 'doc'
                 }
@@ -517,7 +130,7 @@ module.exports = function ( grunt ) {
             all: {
                 options: {
                     timeout: 60000,
-                    urls: [ 'http://localhost:9999/index.html' ]
+                    urls: ['http://localhost:9999/index.html']
                 }
             }
         },
@@ -526,7 +139,7 @@ module.exports = function ( grunt ) {
             all: {
                 options: {
                     testname: 'asmcrypto.js',
-                    urls: [ 'http://localhost:9999/' ],
+                    urls: ['http://localhost:9999/'],
                     browsers: browsers,
                     build: process.env.TRAVIS_JOB_ID,
                     'max-duration': 600
@@ -539,7 +152,7 @@ module.exports = function ( grunt ) {
                 options: {
                     hostname: 'localhost',
                     port: 9999,
-                    base: [ 'test', '.' ],
+                    base: ['test', '.'],
                     directory: 'test'
                 }
             }
@@ -547,21 +160,22 @@ module.exports = function ( grunt ) {
 
         watch: {
             all: {
-                files: '<%= sources.files %>',
-                tasks: ['sources','concat']
+                files: 'src/**/*.js',
+                tasks: ['rollup:all', 'rollup:test_AES_ASM']
             }
         },
 
         clean: [
             'asmcrypto.js',
             'asmcrypto.js.map',
+            'test/aes.asm.js',
+            'test/aes.asm.js.map',
             'doc/'
         ]
     });
 
-    grunt.registerTask('sources', sources);
-    grunt.registerTask('default', ['sources','concat','uglify']);
-    grunt.registerTask('devel', ['sources','concat','connect','watch']);
-    grunt.registerTask('test', ['connect','qunit']);
-    grunt.registerTask('sauce', ['connect','saucelabs-qunit']);
+    grunt.registerTask('default', ['rollup:default', 'rollup:test_AES_ASM', 'uglify']);
+    grunt.registerTask('devel', ['rollup:all', 'rollup:test_AES_ASM', 'connect', 'watch']);
+    grunt.registerTask('test', ['connect', 'qunit']);
+    grunt.registerTask('sauce', ['connect', 'saucelabs-qunit']);
 };
