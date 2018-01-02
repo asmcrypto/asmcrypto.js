a6efeddad37ad27aa1dccfe9f8bc8b597c3ca200
Adam Lippai <adam@rigo.sk>
2018-01-02 23:09:10 +0100

    AES ECB should be only used on real random AES size blocks. No padding, no not streched/derived messages.

--- src/aes/ecb/exports.js
+++ src/aes/ecb/exports.js
@@ -5,16 +5,16 @@
 import {_AES_asm_instance, _AES_heap_instance} from '../exports';
 import {AES_ECB, AES_ECB_Decrypt, AES_ECB_Encrypt} from './ecb';
 
-function AES_ECB_encrypt_bytes ( data, key, padding ) {
+function AES_ECB_encrypt_bytes ( data, key ) {
     if ( data === undefined ) throw new SyntaxError("data required");
     if ( key === undefined ) throw new SyntaxError("key required");
-    return new AES_ECB( key, padding, _AES_heap_instance, _AES_asm_instance).encrypt(data).result;
+    return new AES_ECB( key, _AES_heap_instance, _AES_asm_instance).encrypt(data).result;
 }
 
-function AES_ECB_decrypt_bytes ( data, key, padding ) {
+function AES_ECB_decrypt_bytes ( data, key ) {
     if ( data === undefined ) throw new SyntaxError("data required");
     if ( key === undefined ) throw new SyntaxError("key required");
-    return new AES_ECB( key, padding, _AES_heap_instance, _AES_asm_instance ).decrypt(data).result;
+    return new AES_ECB( key, _AES_heap_instance, _AES_asm_instance ).decrypt(data).result;
 }
 
 
